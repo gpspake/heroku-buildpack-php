e5682f93cbd28ae45a021dffee94ea5fa83a5487
George Spake <gpspake@gmail.com>
2017-01-06 14:35:35 -0600

    uncomment log config

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -6,7 +6,7 @@ http {
     #                  '$status $body_bytes_sent "$http_referer" '
     #                  '"$http_user_agent" "$http_x_forwarded_for"';
 
-    #access_log  logs/access.log  main;
+    access_log  logs/access.log  main;
     
     client_max_body_size 20m;
 
